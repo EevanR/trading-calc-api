f5f8802b059590f4034072a315e1513cceef5b3e
Eevan <eevan.redon@gmail.com>
2020-04-16 17:22:10 +0200

    adds create action in Trades controller

--- app/controllers/api/v1/trades_controller.rb
+++ app/controllers/api/v1/trades_controller.rb
@@ -5,5 +5,18 @@ class Api::V1::TradesController < ApplicationController
   end
 
   def create
+    trade = Trade.create(trade_params)
+
+    if trade.persisted?
+      render json: trade
+    else
+      render json: { error: trade.errors.full_messages }, status: 422
+    end
+  end
+
+  private
+
+  def trade_params
+    params.require(:trade).permit(:ticker, :entry, :shares, :stop, :setup, :date, :profit, :trade_id)
   end
 end
---
+++ spec/requests/api/v1/trades/create_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+RSpec.describe 'POST /api/v1/trades', type: :request do
+  let!(:headers) { { HTTP_ACCEPT: 'application/json' } }
+
+  describe 'Succesfully creates trade entry' do
+    before do
+      post '/api/v1/trades',
+      params: { 
+        trade: {
+          ticker: "AMZN",
+          entry: 1800.98,
+          shares: 40,
+          stop: 1800.00,
+          setup: "Setup 1",
+          date: "20/01/01",
+          profit: 1000.00,
+          trade_id: 2
+        }
+      }, 
+      headers: headers
+    end
+
+    it 'returns a 200 response status' do
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'Unsuccesfully creates trade entry with missing param' do
+    before do
+      post '/api/v1/trades',
+      params: { 
+        trade: {
+          ticker: nil,
+          entry: 1800.98,
+          shares: 40,
+          stop: 1800.00,
+          setup: "Setup 1",
+          date: "20/01/01",
+          profit: 1000.00,
+          trade_id: 2
+        }
+      }, 
+      headers: headers
+    end
+
+    it 'returns a 422 response status' do
+      expect(response).to have_http_status 422
+    end
+
+    it 'returns an error message' do
+      expect(response_json['error'][0]).to eq "Ticker can't be blank"
+    end
+  end
+end
