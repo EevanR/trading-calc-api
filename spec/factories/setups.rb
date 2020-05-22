eb0c30d1fef665afc559a2de883056de65d43d8a
Eevan <eevan.redon@gmail.com>
2020-05-22 23:40:21 +0200

    adds Setup model

---
+++ spec/factories/setups.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :setup do
+    reqOne { "MyString" }
+    reqTwo { "MyString" }
+    reqThree { "MyString" }
+    reqFour { "MyString" }
+    reqFive { "MyString" }
+    reqSix { "MyString" }
+    reqSeven { "MyString" }
+    reqEight { "MyString" }
+    reqNine { "MyString" }
+    reqTen { "MyString" }
+  end
+end
