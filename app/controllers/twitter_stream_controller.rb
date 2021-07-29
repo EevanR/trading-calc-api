105f999389403bbc20c595a204500f7569ceaf11
Eevan <eevan.redon@gmail.com>
2021-07-29 10:28:47 +0200

    creates twitter_stream controller

---
+++ app/controllers/twitter_stream_controller.rb
@@ -0,0 +1,16 @@
+class TwitterStreamController < ApplicationController
+  def get_all_rules
+  end
+
+  def set_rules
+  end
+
+  def delete_all_rules
+  end
+
+  def setup_rules
+  end
+
+  def stream_connect
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'twitter_stream/get_all_rules'
+  get 'twitter_stream/set_rules'
+  get 'twitter_stream/delete_all_rules'
+  get 'twitter_stream/setup_rules'
+  get 'twitter_stream/stream_connect'
+  get 'get_all_rules/set_rules'
+  get 'get_all_rules/delete_all_rules'
+  get 'get_all_rules/setup_rules'
   get 'users/update'
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
   namespace :api do
