28197e01ee21fb0667dbe639433fb8565c1c3f52
Eevan <eevan.redon@gmail.com>
2021-08-04 21:17:06 +0200

    adds update action, tweets controller

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -29,10 +29,23 @@ class Api::V1::TweetsController < ApplicationController
     end
   end
 
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(update_params)
+    if tweet.persisted?
+      render json: tweet
+    else
+      render json: { error: tweet.errors.full_messages }, status: 422
+    end
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:name)
   end
 
+  def update_params
+    params.require(:tweet).permit(:name)
+  end
 end
