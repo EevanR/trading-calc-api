480118c8daf06436ab3f844fa31af5c9af9f07e8
Eevan <eevan.redon@gmail.com>
2022-04-23 09:57:33 +0200

    updates excels factory to containt more data points

--- spec/factories/excels.rb
+++ spec/factories/excels.rb
@@ -1,7 +1,24 @@
 FactoryBot.define do
   factory :excel do
     user_id { 1 }
-    data { [{:Ticker=>"AMC", :NetProfit=>23.44}, {:Ticker=>"FB", :NetProfit=>23.0}] }
+    data { 
+      [
+        {
+          :Ticker=>"AMC", 
+          :NetProfit=>23.44,
+          :GrossProfit=>25.80,
+          :ShareCount=> 0,
+          :Commissions=>2.36
+        }, 
+        {
+          :Ticker=>"FB", 
+          :NetProfit=>23.0,
+          :GrossProfit=>26.80,
+          :ShareCount=> 0,
+          :Commissions=>3.8
+        }
+      ] 
+    }
     fees { 78 }
   end
 end
