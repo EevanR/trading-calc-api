353944a24f2b3724de8990b31d837d9d20d5846f
Eevan <eevan.redon@gmail.com>
2022-04-30 09:55:38 +0200

    update excels.data only returns 10 latest entries

--- spec/requests/api/v1/excel/update_spec.rb
+++ spec/requests/api/v1/excel/update_spec.rb
@@ -44,4 +44,43 @@ RSpec.describe 'PATCH /api/v1/excels/:id', type: :request do
       expect(response_json['data'][0]['NetProfit']).to eq "234234"
     end
   end
+
+  describe 'Succesfully return limited updated trade data when not subscriber' do
+    before do
+      patch "/api/v1/excels/#{excel.id}",
+      params: {
+        data: [
+          {Ticker: "AMC", NetProfit: 234234},
+          {Ticker: "FB", NetProfit: 23.00},
+          {Ticker: "AMZN", NetProfit: 234234},
+          {Ticker: "AAPL", NetProfit: 23.00},
+          {Ticker: "MSFT", NetProfit: 234234},
+          {Ticker: "YTEN", NetProfit: 23.00},
+          {Ticker: "GSUM", NetProfit: 234234},
+          {Ticker: "CASA", NetProfit: 23.00},
+          {Ticker: "SAVA", NetProfit: 234234},
+          {Ticker: "FRGE", NetProfit: 23.00},
+          {Ticker: "GME", NetProfit: 234234},
+          {Ticker: "DRYS", NetProfit: 1000000.00},
+        ]
+      },
+      headers: headers
+    end
+
+    it 'returns a 200 response status' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'returns updated ticker data' do
+      expect(response_json['data'][9]['NetProfit']).to eq "1000000.0"
+    end
+
+    it 'returns limited data length to 10 entries' do
+      expect(response_json['data'].length).to eq 10
+    end
+
+    it 'returns most previous data entries' do
+      expect(response_json['data'][0]['Ticker']).to eq "AMZN"
+    end
+  end
 end
