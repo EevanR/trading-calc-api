69eb2050a836d72305423da8958cd65498b8e5d0
Eevan <eevan.redon@gmail.com>
2022-05-11 05:37:57 +0200

    adds subscriptions create function, adds success and failure tests to subscription_spec.rb

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,7 +1,34 @@
 class Api::V1::SubscriptionsController < ApplicationController
-  before_action :authenticate_current_user
+  before_action :authenticate_user!
   rescue_from Stripe::InvalidRequestError, with: :invalid_token_id
   
   def create
+    if params[:stripeToken]
+      customer = Stripe::Customer.create(
+        email: current_user.email,
+        source: params[:stripeToken]
+      )
+
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        currency: 'sek',
+        amount: 999,
+        description: 'Monthly Subscription'
+      )
+      
+      if (charge.paid)
+        current_user.role = 'subscriber'
+        current_user.save
+        render json: { message: 'Transaction cleared'}
+      end
+    else
+      render json: { message: 'No Stripe token detected'}
+    end
   end
+
+  private
+
+  def invalid_token_id
+    render json: { message: 'Transaction rejected, token invalid'}
+  end 
 end
--- spec/requests/api/v1/subscription_spec.rb
+++ spec/requests/api/v1/subscription_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'User can buy subscritption' do
   let(:stripe_helper) { StripeMock.create_test_helper }
     before(:each) { StripeMock.start }
     after(:each) { StripeMock.stop }
-  let(:user) { create(:user) }
+  let(:user) { create(:user, role: "user") }
   let(:user_credentials) { user.create_new_auth_token }
   let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials) }
 
@@ -29,6 +29,54 @@ RSpec.describe 'User can buy subscritption' do
 
       it 'has their role updated to subscriber' do
         expect(user.role).to eq 'subscriber'
+      end
+    end
+  end
+
+  describe "unsuccessfully" do
+    describe 'with invalid stripe token' do
+      before do
+        post '/api/v1/subscriptions',
+        params: {
+          stripeToken: 123456789    
+        },
+        headers: headers
+      end
+
+      it "recieves message 'Transaction rejected, token invalid'" do
+        expect(response_json["message"]).to eq 'Transaction rejected, token invalid'
+      end
+
+      it 'has their role remain at user status' do
+        expect(user.role).to eq 'user'
+      end
+    end
+
+    describe 'with no stripe token' do
+      before do
+        post '/api/v1/subscriptions',
+        headers: headers
+      end
+
+      it "recieves 'No Stripe token detected'" do
+        expect(response_json["message"]).to eq 'No Stripe token detected'
+      end
+    end
+
+    describe 'when user is not signed in' do
+      before do
+        post '/api/v1/subscriptions',
+          params: {
+            stripeToken: stripe_helper.generate_card_token    
+          }
+      end
+
+      it 'returns unsuccessful response' do
+        expect(response).to have_http_status 401
+      end
+
+      it 'returns message to sign in or register first' do
+        expect(response_json["errors"][0]).to eq 'You need to sign in or sign up before continuing.'
       end
     end
   end
