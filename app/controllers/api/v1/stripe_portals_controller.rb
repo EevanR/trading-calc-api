1a756091336a056f1a1c86a0a0a5ad688f96990a
Eevan <eevan.redon@gmail.com>
2022-06-16 21:00:19 +0200

    updates portal create to accept request when no stripe session just logged in user

--- app/controllers/api/v1/stripe_portals_controller.rb
+++ app/controllers/api/v1/stripe_portals_controller.rb
@@ -2,21 +2,21 @@ class Api::V1::StripePortalsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    session = StripeSession.find_by(session_id: params['session_id'])
-    user = session.user
     return_url = 'http://localhost:3001/panes'
 
-    if user.customer_id === current_user.customer_id
-      binding.pry
-      session = Stripe::BillingPortal::Session.create({
-                                                        customer: user.customer_id,
-                                                        return_url: return_url
-                                                      })
-      # render json: session, status: 200
-      
-      redirect_to session.url, status: 303
+    if params['session_id']
+      session = StripeSession.find_by(session_id: params['session_id'])
+      session.user.customer_id === current_user.customer_id
+      user = session.user
     else
-      render json: { errors: "Not Authorized" }, status: 401
+      user = current_user
     end
+
+    session = Stripe::BillingPortal::Session.create({
+      customer: user.customer_id,
+      return_url: return_url
+    })
+    
+    redirect_to session.url, status: 301
   end
 end
