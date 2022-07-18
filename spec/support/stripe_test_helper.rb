aace1ec8c48d1a7ecea988c879c8839f08df7f42
Eevan <eevan.redon@gmail.com>
2022-07-18 10:51:38 +0200

    updates webhook secret

--- spec/support/stripe_test_helper.rb
+++ spec/support/stripe_test_helper.rb
@@ -3,7 +3,7 @@
 module StripeTestHelper
   def stripe_event_signature(payload)
     time = Time.now
-    secret = Rails.application.credentials.stripe_webhook_secret
+    secret = "whsec_5q5HfgFpL6Xmsl4yEke9NFLMy6twRTlB"
     signature = Stripe::Webhook::Signature.compute_signature(time, payload, secret)
     Stripe::Webhook::Signature.generate_header(
       time,
